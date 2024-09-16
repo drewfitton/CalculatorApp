73fe2ef1d9de987552f8a887e65e5a1ed96eea1b
Fitton, Andrew David <afitton3@gatech.edu>
2024-09-16 17:45:34 -0400

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by drewf on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
